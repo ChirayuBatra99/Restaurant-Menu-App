6eadaf7d48d0cfd6584eb6a78ed3539a91853884
chirayubatra1104 <chirayubatra1104@gmail.com>
2024-06-25 18:17:06 +0530

    Adding changes 25 June

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import {useSelector} from 'react-redux'
 import styles from './Headers.module.scss'
+
 function Headers() {
     const navigate = useNavigate();
     const {carts} = useSelector((state)=>state.allCart);
@@ -13,13 +14,14 @@ function Headers() {
   return (
     <div className={styles.header}>
         <div className={styles.textHeader}  onClick={()=> navigate('/')}>
-            Menu 
+            Menu
         </div>
-        <div className='button-cart'>
-            <button onClick={goToCart}>Cart</button>
+        <div className={styles.header}>
+        <div>
+            <button className={styles.buttonCart} onClick={goToCart}>🛒</button>
         </div>
         <p> Items in cart : {carts.length} </p> 
-
+        </div>
     </div>
   )
 }
